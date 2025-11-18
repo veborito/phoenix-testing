8050d19743c831faa3ab205ec098a193d215dcc9
Boris Verdecia Echarte <borisverdeciaecharte@gmail.com>
2025-11-18 16:54:39 +0100

    feat: Set up gigalixir deployment with elixir releases v5

--- config/runtime.exs
+++ config/runtime.exs
@@ -56,9 +56,15 @@ if config_env() == :prod do
   config :hello_app, :dns_cluster_query, System.get_env("DNS_CLUSTER_QUERY")
 
   config :hello_app, HelloAppWeb.Endpoint,
-    server: true,
-    url: [host: System.get_env("APP_NAME") <> ".gigalixirapp.com", port: 443],
-    http: [port: {:system, "PORT"}],
+    url: [host: host, port: 443, scheme: "https"],
+    http: [
+      # Enable IPv6 and bind on all interfaces.
+      # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
+      # See the documentation on https://hexdocs.pm/bandit/Bandit.html#t:options/0
+      # for details about using IPv6 vs IPv4 and loopback vs public addresses.
+      ip: {0, 0, 0, 0, 0, 0, 0, 0},
+      port: port
+    ],
     secret_key_base: secret_key_base
 
   # ## SSL Support
